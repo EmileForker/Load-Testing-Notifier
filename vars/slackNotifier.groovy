a779bb66565485d34714db01dfab045869cd7271
Emil Taniverdiev <business.high@gmail.com>
2018-08-21 15:05:16 +0300

    embed

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,7 +2,7 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was successful :white_check_mark: <${env.BUILD_URL}|Open Job>"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber <${env.BUILD_URL}|${env.BUILD_NUMBER}> was successful :white_check_mark:"
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was failed :crossed_swords: <${env.BUILD_URL}|Open Job>"
