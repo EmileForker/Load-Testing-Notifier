73043df5a17032fe918acc26239d7078d650616c
Emil Taniverdiev <business.high@gmail.com>
2018-08-21 14:49:25 +0300

    final_fix

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,15 +2,15 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was successful :fire: (<${env.BUILD_URL}|Open>)"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was successful :white_check_mark: <${env.BUILD_URL}|Open Job>"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was failed. ${env.BUILD_URL}"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was failed :crossed_swords: <${env.BUILD_URL}|Open Job>"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was unstable. ${env.BUILD_URL}"
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was unstable :fire: <${env.BUILD_URL}|Open Job>"
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was unclear. ${env.BUILD_URL}"	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} performed on Scrum: ${env.SCRUM_NUMBER} with buildnumber ${env.BUILD_NUMBER} was unclear :fire: <${env.BUILD_URL}|Open Job>"	
   }
 }
